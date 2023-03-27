8f8d89c0e6c04eeabbe212f4fae6db25a9cb70c4
Akihiko Okubo <can.tago.mago524@gmail.com>
2023-03-27 10:49:01 +0900

    poc

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// https://github.com/GoogleCloudPlatform/golang-samples/blob/HEAD/run/helloworld/main.go
+
+func main() {
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
+
+	// Secret managerのdemo
+	sc := os.Getenv("SECRET")
+	fmt.Fprintf(w, "SECRET %s!\n", sc)
+
+}
