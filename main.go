6d2710baf13f603379c9ea052eb1ea85ddeec70e
Akihiko Okubo <can.tago.mago524@gmail.com>
2023-04-02 15:55:06 +0900

    add sample code

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	fb "firebase.google.com/go"
 )
 
 // https://github.com/GoogleCloudPlatform/golang-samples/blob/HEAD/run/helloworld/main.go
-
 func main() {
 	log.Print("starting server...")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/firestore", firestoreTest)
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
@@ -35,7 +38,38 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!\n", name)
 
 	// Secret managerのdemo
-	sc := os.Getenv("SECRET")
-	fmt.Fprintf(w, "SECRET %s!\n", sc)
+	sc := os.Getenv("TEST_SECRET")
+	if sc != "" {
+		fmt.Fprintf(w, "TEST_SECRET_VALUE:  %s!\n", sc)
+	}
+}
 
+// firestoreのdemo
+func firestoreTest(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	conf := &fb.Config{ProjectID: os.Getenv("PROJECT_ID")}
+	app, err := fb.NewApp(ctx, conf)
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	type Hoge struct {
+		Val string
+	}
+
+	_, err = client.Collection("testCollection").Doc(time.Now().String()).Create(ctx, &Hoge{
+		Val: "test_val",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "ok")
+	return
 }
